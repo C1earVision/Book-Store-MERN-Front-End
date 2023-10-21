9393f9da1e71a8f5d284dae51e860d76d605c0b4
C1earVision <ahmedebrahim3690@gmail.com>
2023-10-21 16:16:08 +0300

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <footer className='h-[120px] bg-[rgb(34,34,34)] mt-[30px]'>
+    <footer className='h-[100%px] bg-[rgb(34,34,34)] mt-[30px]'>
       <div className='h-[40px] bg-[rgb(145,63,226)] items-center flex flex-row justify-center flex-wrap gap-10 text-white'>
-        <h1>Privacy Policy</h1>
-        <h1>Terms Of Service</h1>
         <Link to='https://github.com/C1earVision'><i class="fa-brands fa-github fa-2x"></i></Link>
         <h1>Design By C1earVision &copy; </h1>
+      </div>
+      <div className='flex justify-center p-4 h-[100%] text-white'>
+        <i className="fa-solid self-center fa-book-open-reader fa-3x"></i>
       </div>
     </footer>
   )
