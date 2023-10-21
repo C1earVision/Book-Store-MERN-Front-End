4091f36ed4f2ee2b9ff4744e1f24dee1824c2eba
C1earVision <ahmedebrahim3690@gmail.com>
2023-10-21 17:21:47 +0300

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="lg:w-[70%] h-[500px] justify-center flex md:w-[90%] text-white sm:w-[90%] w-[100%] m-auto bg-[rgb(34,34,34)] mt-[2%]">
       <div className='w-[90%] self-center'>
-        <p>This Website Is a college peoject made by me i.e <strong><Link to="https://github.com/C1earVision">C1earVision&copy;</Link></strong> and its design was inspired by <strong><Link to='https://asuratoon.com/'>https://asuratoon.com/</Link></strong></p>
+        <p>This Website Is a college peoject made by me i.e. <strong><Link to="https://github.com/C1earVision">C1earVision&copy;</Link></strong> and its design was inspired by <strong><Link to='https://asuratoon.com/'>https://asuratoon.com/</Link></strong></p>
       </div>
     </div>
   )
