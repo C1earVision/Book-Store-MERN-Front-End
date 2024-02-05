1050521568af3124bd31aabfb77e7b90e38d8b86
C1earVision <133123242+C1earVision@users.noreply.github.com>
2024-02-05 15:31:00 +0200

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="lg:w-[70%] h-[500px] justify-center flex md:w-[90%] text-white sm:w-[90%] w-[100%] m-auto bg-[rgb(49,71,106)] rounded-lg mt-[2%]">
       <div className='w-[90%] self-center'>
-        <p>This website is a college peoject made by me i.e. <strong><Link to="https://github.com/C1earVision">C1earVision&copy;</Link></strong> and its design was inspired by <strong><Link to='https://www.aseeralkotb.com/en'>https://www.aseeralkotb.com/en</Link></strong></p>
+        <p>This website is made by me i.e. <strong><Link to="https://github.com/C1earVision">C1earVision&copy;</Link></strong> and its design was inspired by <strong><Link to='https://www.aseeralkotb.com/en'>https://www.aseeralkotb.com/en</Link></strong></p>
       </div>
     </div>
   )
